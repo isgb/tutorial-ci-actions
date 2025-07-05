3bb1c1c0bf619784f336a8b27a8c78f20b922f18
isaias <isgb_codigo@hotmail.com>
2025-07-05 17:15:59 -0600

    Wip

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
+BRANCH=$1
+
+if[ "$BRANCH" == "main" ]; then
+   DEPLOY_PATH="/home/azureuser/tutorial-ci-actions"
+else
+   DEPLOY_PATH="/home/azureuser/stage"
+fi
+
 ssh -o StrictHostKeyChecking=no azureuser@20.55.25.65 <<HTML
 
     cd /home/azureuser/tutorial-ci-actions
 
-    git pull --rebase origin main
+    git pull --rebase origin $BRANCH
 
 HTML
